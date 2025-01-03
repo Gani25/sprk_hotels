16a03aa8ccadaf64beb8f96232104bffdf9de275
Gani25 <memonabdulgani3d@gmail.com>
2025-01-03 20:59:57 +0530

    Add Listing Form

--- src/main/java/com/sprk/sprk_hotels/controller/HotelController.java
+++ src/main/java/com/sprk/sprk_hotels/controller/HotelController.java
@@ -1,8 +1,37 @@
 package com.sprk.sprk_hotels.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.sprk.sprk_hotels.model.Listing;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HotelController {
+
+    @GetMapping("/listings/add")
+    public String showAddForm(Model model) {
+        Listing listing = new Listing();
+        model.addAttribute("listing", listing);
+        return "listing-form";
+    }
+
+    @PostMapping("/listings")
+    public String saveListing(@Valid @ModelAttribute Listing listing, BindingResult bindingResult) {
+        // Handling Server Side validation
+        if (bindingResult.hasErrors()) {
+            System.out.println(listing);
+            return "listing-form";
+        }
+
+        return "index";
+    }
 
 }
--- src/main/java/com/sprk/sprk_hotels/model/Listing.java
+++ src/main/java/com/sprk/sprk_hotels/model/Listing.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.ToString;
 
 @Entity
 @Data
+@ToString
 public class Listing {
 
     @Id
@@ -17,13 +21,17 @@ public class Listing {
     private int id;
 
     @NotBlank(message = "Please enter title")
-    @Size(min = 5, message = "Please ensure correct length")
+    @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
     private String title;
 
+    @NotBlank(message = "Please enter description")
+    @Size(min = 10, max = 255, message = "Description must be between 10 and 255 characters")
     private String description;
 
+    @NotBlank(message = "Please enter image URL")
     private String imageUrl;
 
+    @Positive(message = "Price must be a positive number")
     private int price;
 
 }
